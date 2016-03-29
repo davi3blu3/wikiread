e7c08214a2a179cffd21bbd1e69acf182884d8f5
Dave Harned <dharned@gmail.com>
2016-03-29 12:54:34 -0500

    resolve event error, clear previous search results

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
-
-$(document).ready( function() {
+$(document).ready(function () {
+    'use strict';
 
     var container = $('.search-wrapper'),
         textInput;
@@ -10,55 +9,60 @@ $(document).ready( function() {
     
     // GET request to WikiPedia, and display result to DOM
     function searchResult(query) {
-        var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&namespace=0&format=json';
+        var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&namespace=0&format=json',
+            i;
+        
+        // clear previous search results, if any
+        if ($('.article-frame').length > 0) {
+            $('.article-frame').remove();
+        };
+        
+        // API call
         $.ajax({
-           type: 'GET',
+            type: 'GET',
             url: url,
             async: false,
             contentType: "application/json",
             dataType: 'jsonp',
             success: function (data) {
                 // iterate through variables, push to DOM
-                for (i = 0; i < data[1].length; i++) {
+                for (i = 0; i < data[1].length; i += 1) {
                     $('.results').append('<div class="article-frame"><a href="' + data[3][i] + '"><h3 class="article-title">' + data[1][i] + '</h3></a><p class="article-body">' + data[2][i] + '</p></div>');
                 }
                 // clear input
                 container.find('.search-input').val('');
-            },
+            }
         });
     }
 
     // open search field, or if open, launch search
-    $('.search-icon').click(function(event){
+    $('.search-icon').click(function (event) {
         textInput = $('.search-input').val();
 
-        if(!container.hasClass('active')){
+        if (!container.hasClass('active')) {
             console.log("search open!");
             container.addClass('active');
             event.preventDefault();
-        } else if (container.hasClass('active') && textInput.length > 0){
-
+        } else if (container.hasClass('active') && textInput.length > 0) {
             console.log("search for: " + textInput);
-            searchResult(textInput);          
+            searchResult(textInput);
         }
     });
 
-
-    $(document).keypress(function(e) {
-        if(e.which == 13 && container.hasClass('active')) {
-            console.log('You pressed enter!');
+    // launch search with 'enter' key if text entered
+    $(document).keypress(function (e) {
+        if (e.which === 13 && container.hasClass('active')) {
             textInput = $('.search-input').val();
             if (textInput.length > 0) {
                 console.log("search for: " + textInput);
-                searchResult(textInput);  
+                searchResult(textInput);
             }
         }
     });
 
-
-    // close search field
-    $('.close').click(function(){
-        if(container.hasClass('active') && $('.close').closest('.input-holder').length == 0){
+    // close search field on clicking X
+    $('.close').click(function () {
+        if (container.hasClass('active') && $('.close').closest('.input-holder').length === 0) {
             console.log("search close!");
             container.removeClass('active');
             // clear input
@@ -66,7 +70,5 @@ $(document).ready( function() {
         }
     });
 
-
-
-})
+});
 
