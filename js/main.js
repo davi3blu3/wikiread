1f54db328423dfbc83dd02b6374534d4a0a94eb4
Dave Harned <dharned@gmail.com>
2016-03-29 11:44:56 -0500

    activate enter key event

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(document).ready( function() {
     var container = $('.search-wrapper'),
         textInput;
     
+    // clear previous input, if any
+    container.find('.search-input').val('');
+    
     // GET request to WikiPedia, and display result to DOM
     function searchResult(query) {
         var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&namespace=0&format=json';
@@ -15,6 +18,7 @@ $(document).ready( function() {
             contentType: "application/json",
             dataType: 'jsonp',
             success: function (data) {
+                // iterate through variables, push to DOM
                 for (i = 0; i < data[1].length; i++) {
                     $('.results').append('<div class="article-frame"><a href="' + data[3][i] + '"><h3 class="article-title">' + data[1][i] + '</h3></a><p class="article-body">' + data[2][i] + '</p></div>');
                 }
@@ -25,7 +29,7 @@ $(document).ready( function() {
     }
 
     // open search field, or if open, launch search
-    $('.search-icon').click(function(){
+    $('.search-icon').click(function(event){
         textInput = $('.search-input').val();
 
         if(!container.hasClass('active')){
@@ -39,11 +43,19 @@ $(document).ready( function() {
         }
     });
 
+
     $(document).keypress(function(e) {
-        if(e.which == 13) {
-            alert('You pressed enter!');
+        if(e.which == 13 && container.hasClass('active')) {
+            console.log('You pressed enter!');
+            textInput = $('.search-input').val();
+            if (textInput.length > 0) {
+                console.log("search for: " + textInput);
+                searchResult(textInput);  
+            }
         }
     });
+
+
     // close search field
     $('.close').click(function(){
         if(container.hasClass('active') && $('.close').closest('.input-holder').length == 0){
