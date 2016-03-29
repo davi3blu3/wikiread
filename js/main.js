1097725119556d21c6a47655a8b37303f1038b28
Dave Harned <dharned@gmail.com>
2016-03-29 17:56:40 -0500

    add random button and api call

--- js/main.js
+++ js/main.js
@@ -25,18 +25,55 @@ $(document).ready(function () {
             contentType: "application/json",
             dataType: 'jsonp',
             success: function (data) {
-
+                console.log('random call made');
                 // iterate through results, push to DOM
                 for (i = 0; i < data[1].length; i += 1) {
             
                     $('<div class="article-frame"><a href="' + data[3][i] + '"><h3 class="article-title">' + data[1][i] + '</h3></a><p class="article-body">' + data[2][i] + '</p></div>').hide().appendTo(".results").fadeIn(500);
-            
                 }
                 // clear input
                 container.find('.search-input').val('');
             }
         });
     }
+    
+    // GET request for random article, display result to DOM
+    function randomResult () {
+        var url = 'https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&grnlimit=1&prop=info&inprop=url&format=json',
+            i;
+        
+        // clear previous search results, if any
+        if ($('.article-frame').length > 0) {
+            $('.article-frame').remove();
+        }
+        
+        // API call
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: false,
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function (data) {
+
+                // iterate through results, push to DOM
+                var pageID = Object.keys(data.query.pages)[0];
+                var title = data.query.pages[pageID].title;
+                var link = data.query.pages[pageID].fullurl;
+                console.log(title + " " + link);
+            
+                $('<div class="article-frame"><a href="' + link + '"><h3 class="article-title">' + title + '</h3></a>').hide().appendTo(".results").fadeIn(500);
+            
+                // clear input
+                container.find('.search-input').val('');
+            }
+        });
+    }
+
+    // random search
+    $('.random').click(function () {
+       randomResult(); 
+    });
 
     // open search field, or if open, launch search
     $('.search-icon').click(function (event) {
@@ -74,3 +111,49 @@ $(document).ready(function () {
     });
 
 });
+
+
+
+//https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&grnlimit=5&prop=info&inprop=url
+//
+//{
+//    "batchcomplete": "",
+//    "continue": {
+//        "grncontinue": "0.046177707503|0.046179792336|29235722|0",
+//        "continue": "grncontinue||"
+//    },
+//    "query": {
+//        "pages": {
+//            "3100922": {
+//                "pageid": 3100922,
+//                "ns": 0,
+//                "title": "Harry R\u00e9e",
+//                "contentmodel": "wikitext",
+//                "pagelanguage": "en",
+//                "pagelanguagehtmlcode": "en",
+//                "pagelanguagedir": "ltr",
+//                "touched": "2016-02-22T00:50:12Z",
+//                "lastrevid": 706193550,
+//                "length": 3865,
+//                "fullurl": "https://en.wikipedia.org/wiki/Harry_R%C3%A9e",
+//                "editurl": "https://en.wikipedia.org/w/index.php?title=Harry_R%C3%A9e&action=edit",
+//                "canonicalurl": "https://en.wikipedia.org/wiki/Harry_R%C3%A9e"
+//            },
+//            "48901872": {
+//                "pageid": 48901872,
+//                "ns": 0,
+//                "title": "Chemill\u00e9-en-Anjou",
+//                "contentmodel": "wikitext",
+//                "pagelanguage": "en",
+//                "pagelanguagehtmlcode": "en",
+//                "pagelanguagedir": "ltr",
+//                "touched": "2016-03-28T12:15:51Z",
+//                "lastrevid": 705801369,
+//                "length": 1755,
+//                "fullurl": "https://en.wikipedia.org/wiki/Chemill%C3%A9-en-Anjou",
+//                "editurl": "https://en.wikipedia.org/w/index.php?title=Chemill%C3%A9-en-Anjou&action=edit",
+//                "canonicalurl": "https://en.wikipedia.org/wiki/Chemill%C3%A9-en-Anjou"
+//            }
+//        }
+//    }
+//}
